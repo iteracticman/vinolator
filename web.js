799553e7f73b665fec74e2112ecdad44fe0b80ee
Peter Weishapl <pw@tailored-apps.com>
2013-11-22 10:52:47 +0100

    send index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+  response.sendfile('index.html');
 });
 
 var port = process.env.PORT || 5000;
